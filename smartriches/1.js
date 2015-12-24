e05dde3af4b2c88d4e4980decbd78c00642412e6
appleclubdesign <3202996537@qq.com>
2015-12-24 09:18:30 +0800

    1
    1

---
+++ smartriches/1.js
@@ -0,0 +1,19 @@
+﻿
+
+function a()
+	{
+		document.getElementById("fanye").src="images/left2.png" ;
+	}
+function b()
+	{
+		document.getElementById("fanye").src="images/left.png" ;
+	}
+
+$(document).ready(function(){
+
+	$("#button1").click(function(){
+	$("pre").toggle("slow");
+	});
+
+	
+});
